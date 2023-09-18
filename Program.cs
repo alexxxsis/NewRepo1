60c80fd320da49a842ff1e171fda2194b0b737c3
Mona Lisa <email@example.com>
2023-09-18 10:04:22 +0800

    123321

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-рра
-    Console.WriteLine("World!");
+Console.WriteLine("World!");
+Console.WriteLine("born to trap!");
